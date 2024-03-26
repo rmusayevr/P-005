7fa50558e251dfed63fbf2d0c3391c21345e8887
Rashad Musayev <rmusayevr19@gmail.com>
2024-03-26 15:23:41 +0400

    javascript fundamentals

---
+++ javascript_fundamentals/script.js
@@ -0,0 +1,50 @@
+// let isConfirmed = confirm("Do you want to start?");
+
+// console.log(isConfirmed);
+
+// if (isConfirmed) {
+//     alert("You confirmed!");
+// } else {
+//     alert("You didn't confirm!");
+// }
+
+// let firstName = prompt("What is your name?");
+// if (firstName === 'Rashad') {
+//     alert(`Your name is Rashad`);
+// } else {
+//     alert(`Your name isn't Rashad. It is ${firstName}!`);
+// }
+
+// let age = 21;
+// if (age === '21') {
+//     console.log(21);
+// }
+
+
+// document.write(age);
+// document.getElementById('h1').innerHTML = 'JS'
+
+
+// const surname = 'Musayev';
+// surname = 'Rashad';
+// let firstName = 'Rashad';
+// firstName = 'Musayev';
+
+var age = 21;
+var age = 23;
+age = 22.2;
+
+function newFunc2() {
+    console.log(age);
+    return age;
+};
+
+let firstName;
+
+console.log(typeof(firstName));
+
+console.log(typeof(newFunc2));
+
+console.log(typeof(age));
+
+console.log(newFunc2());
