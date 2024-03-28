23d96f0ecbb102f3470d39bb3fa14ac3bf7279e4
Rashad Musayev <rmusayevr19@gmail.com>
2024-03-28 15:26:31 +0400

    conditions and loops

--- javascript_fundamentals/script.js
+++ javascript_fundamentals/script.js
@@ -1,12 +1,11 @@
-// let isConfirmed = confirm("Do you want to start?");
-
-// console.log(isConfirmed);
+// let isConfirmed = confirm("Do you want to start?"); // true or false
 
 // if (isConfirmed) {
 //     alert("You confirmed!");
 // } else {
 //     alert("You didn't confirm!");
 // }
+
 
 // let firstName = prompt("What is your name?");
 // if (firstName === 'Rashad') {
@@ -15,36 +14,98 @@
 //     alert(`Your name isn't Rashad. It is ${firstName}!`);
 // }
 
-// let age = 21;
-// if (age === '21') {
-//     console.log(21);
+// let age = '21';
+// if (age === 21) {
+//     console.log(age);
+// } else {
+//     console.log(age + 2);
 // }
 
 
 // document.write(age);
-// document.getElementById('h1').innerHTML = 'JS'
+// document.getElementById('h1').innerHTML += ' JS'
 
 
 // const surname = 'Musayev';
 // surname = 'Rashad';
+// console.log(surname);
+
 // let firstName = 'Rashad';
 // firstName = 'Musayev';
+// console.log(firstName);
 
-var age = 21;
-var age = 23;
-age = 22.2;
+// var age = 21;
+// var age = 23;
+// age = 22.2;
 
-function newFunc2() {
-    console.log(age);
-    return age;
-};
+// console.log(age);
 
-let firstName;
+// function newFunc2() {
+//     console.log(age);
+//     return age;
+// };
 
-console.log(typeof(firstName));
+// let firstName;
 
-console.log(typeof(newFunc2));
+// console.log(typeof(firstName));
 
-console.log(typeof(age));
+// console.log(typeof(newFunc2));
 
-console.log(newFunc2());
+// console.log(typeof(age));
+
+// console.log(newFunc2());
+
+// let day = 'Monday';
+// if (day == 'Monday') {
+//     alert("It's not Thursday. It's Monday.");
+// } else if (day === 'Tuesday') {
+//     alert("It's not Thursday. It's Tuesday.");
+// } else if (day === 'Wednesday') {
+//     alert("It's not Thursday. It's Wednesday.");
+// } else if (day === 'Friday') {
+//     alert("It's not Thursday. It's Friday.");
+// } else if (day === 'Saturday') {
+//     alert("It's not Thursday. It's Saturday.");
+// } else if (day === 'Sunday') {
+//     alert("It's not Thursday. It's Sunday.");
+// } else {
+//     alert(`It's ${day}`);
+// }
+
+// var myNumber = 11;
+
+// while (myNumber < 10) {
+//     console.log(myNumber);
+//     myNumber++;
+// }
+
+// do {
+//     console.log(myNumber);
+//     myNumber++;
+// } while (myNumber < 10);
+
+// let country = 'Sweden';
+// for (let i = 0; i < country.length; i++) {
+//     console.log(country[i]);
+// }
+
+
+// for (let i = 1; i < 5; i++) {
+// console.log("*".repeat(i * 2 - 1));
+// }
+
+// let countries = ['Sweden', 'Finland', 'Norway', 'Iceland'];
+// for (const selectedCountry of countries) {
+//     console.log(selectedCountry);
+// }
+
+// let person = {
+//     "firstName": "Rashad",
+//     "lastName": "Musayev",
+//     "age": 21
+// };
+
+// for (const key in person) {
+//     console.log(`${key}: ${person[key]}`);
+// };
+
